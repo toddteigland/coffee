cc71ab3349554264ed17e39ad01c44a70e30fe05
Todd Teigland <toddteigland@gmail.com>
2023-07-25 11:06:30 -0700

    copied files over from previous

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import logo from "./logo.svg";
+import "./App.css";
+import products from "./products.json";
+import styles from "./styles/home.css";
+
+function App() {
+  return (
+    <div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+      <div className={styles.grid}>
+        {products.map((product) => {
+          return (
+            <div
+              key={product.id} className={styles.card}>
+              <h3>{product.title}</h3>
+              <p>{product.description}</p>
+              <p>${product.price}</p>
+              <p>
+                <button>Add to Cart</button>
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
